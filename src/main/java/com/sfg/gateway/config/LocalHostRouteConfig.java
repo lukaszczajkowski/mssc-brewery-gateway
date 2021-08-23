56242b8865ce60e9847938efb222f16ced33f3c7
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-23 16:14:05 +0200

    Added route to order service

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -12,7 +12,9 @@ public class LocalHostRouteConfig {
     public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                .uri("http://localhost:8080"))
+                    .uri("http://localhost:8080"))
+                .route(r -> r.path("/api/v1/customers/**")
+                    .uri("http://localhost:8081"))
                 .build();
     }
 }
